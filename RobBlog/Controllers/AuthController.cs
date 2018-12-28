972370548d1f903cd67f44a0378f662b0ffb6e57
RobertArthurGillespie <RobertArthurGillespie@outlook.com>
2018-12-28 14:49:19 -0600

    initial commit

---
+++ RobBlog/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using RobBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RobBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //Getting submitted info from login view
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            return Content("Hey there, "+form.Username);
+        }
+    }
+}
